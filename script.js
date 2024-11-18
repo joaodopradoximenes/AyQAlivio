ce097b87cd3154226ec0c1f287c8c4a6c5567fba
João do Prado Ximenes Neto <130679115+joaodopradoximenes@users.noreply.github.com>
2024-11-18 17:53:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const buttonMobile = document.getElementById('button-mobile');
 
+function toggleMenu(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+buttonMobile.addEventListener('click', toggleMenu);
